93c9e1607423f6ac792c588f50ca86c5f4fa39ad
Billy-Elsbury <billy.elsbury@students.ittralee.ie>
2023-02-13 01:55:21 +0000

    Second commit
    add menuItem functions

---
+++ Restaurant System 30.01.2023/MenuItem.cs
@@ -0,0 +1,224 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restuarant_System
+{
+    public class MenuItem
+    {
+        private int itemId;
+        private String type;
+        private String name;
+        private String description;
+        private decimal price;
+
+        public MenuItem()
+        {
+            this.itemId = 0;
+            this.type = "";
+            this.name = "";
+            this.description = "";
+            this.price = 0;
+        }
+
+        public MenuItem(int itemId, string type, string name, string description, decimal price)
+        {
+            this.itemId = itemId;
+            this.type = type;
+            this.name = name;
+            this.description = description;
+            this.price = price;
+        }
+
+        //getters
+        public int getItemId() { return this.itemId; }
+        public String getType() { return this.type; }
+        public String getName() { return this.name; }
+        public String getDescription() { return this.description; }
+        public decimal getPrice() { return this.price; }
+
+        //setters
+        public void setItemId(int ItemId) { itemId = ItemId; }
+        public void setType(String Type) { type = Type; }
+        public void setName(String Name) { name = Name; }
+        public void setDescription(String Description) { description = Description; }
+        public void setPrice(Decimal Price) { price = Price; }
+
+        public static DataSet getAllMenuItems()
+        {
+            //Open a db connection
+            OracleConnection conn = new OracleConnection(DBConnect.oradb);
+
+            //Define the SQL query to be executed
+            String sqlQuery = "SELECT ItemId, , Type, Name, Description, Price " +
+                "FROM Products ORDER BY Name";
+
+            //Execute the SQL query (OracleCommand)
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "menuItem");
+
+            //Close db connection
+            conn.Close();
+
+            return ds;
+        }
+
+        public static DataSet getAllProducts(String Type)
+        {
+            //Open a db connection
+            OracleConnection conn = new OracleConnection(DBConnect.oradb);
+
+            //Define the SQL query to be executed
+            String sqlQuery = "SELECT ProductId, Name, Qty,Price " +
+                "FROM Products WHERE ItemId = '" + Type + "' ORDER BY Name";
+
+            //Execute the SQL query (OracleCommand)
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "menuItem");
+
+            //Close db connection
+            conn.Close();
+
+            return ds;
+        }
+
+        public void getProduct(int Id)
+        {
+            //Open a db connection
+            OracleConnection conn = new OracleConnection(DBConnect.oradb);
+
+            //Define the SQL query to be executed
+            String sqlQuery = "SELECT * FROM Products WHERE ProductID = " + Id;
+
+            //Execute the SQL query (OracleCommand)
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+            conn.Open();
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+
+            //set the instance variables with values from data reader
+            setItemId(dr.GetInt32(0));
+            setType(dr.GetString(1));
+            setName(dr.GetString(1));
+            setDescription(dr.GetString(2));
+            setPrice(dr.GetDecimal(5));
+
+            //close DB
+            conn.Close();
+        }
+
+        public void addProduct()
+        {
+            //Open a db connection
+            OracleConnection conn = new OracleConnection(DBConnect.oradb);
+
+            //Define the SQL query to be executed
+            String sqlQuery = "INSERT INTO Products Values (" +
+                this.itemId + ",'" +
+                this.type + "','" +
+                this.name + "','" +
+                this.description + "','" +
+                this.price + ",')";
+
+            //Execute the SQL query (OracleCommand)
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            //Close db connection
+            conn.Close();
+        }
+
+        public void updateProduct()
+        {
+            //Open a db connection
+            OracleConnection conn = new OracleConnection(DBConnect.oradb);
+
+            //Define the SQL query to be executed
+            String sqlQuery = "UPDATE Products SET " +
+                "ProductId = " + this.itemId + "," +
+                "Name = '" + this.name + "'," +
+                "Type = '" + this.type + "'," +
+                "Description = '" + this.description + "'," +
+                "Price = " + this.price + "," +
+                "WHERE ProductId = " + this.itemId;
+
+            //Execute the SQL query (OracleCommand)
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            //Close db connection
+            conn.Close();
+        }
+
+        public static DataSet findProducts(String itemName)
+        {
+            //Open a db connection
+            OracleConnection conn = new OracleConnection(DBConnect.oradb);
+
+            //Define the SQL query to be executed
+            String sqlQuery = "SELECT ProductId, Name, Manufacturer FROM Products " +
+                "WHERE Name LIKE '%" + itemName + "%' ORDER BY Name";
+
+            //Execute the SQL query (OracleCommand)
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "menuItem");
+
+            //Close db connection
+            conn.Close();
+
+            return ds;
+        }
+
+        public static int getNextmenuItemId()
+        {
+            //Open a db connection
+            OracleConnection conn = new OracleConnection(DBConnect.oradb);
+
+            //Define the SQL query to be executed
+            String sqlQuery = "SELECT MAX(ProductId) FROM Products";
+
+            //Execute the SQL query (OracleCommand)
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+            conn.Open();
+
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            //Does dr contain a value or NULL?
+            int nextId;
+            dr.Read();
+
+            if (dr.IsDBNull(0))
+                nextId = 1;
+            else
+            {
+                nextId = dr.GetInt32(0) + 1;
+            }
+
+            //Close db connection
+            conn.Close();
+
+            return nextId;
+        }
+    }
+}
---
+++ Restaurant System 30.01.2023/frmAddMenuItem.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Restuarant_System
+{
+    public partial class frmAddMenuItem : Form
+    {
+        private Mediator _mediator = new Mediator();
+
+        public frmAddMenuItem()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAddMenuItem_Load(object sender, EventArgs e)
+        {
+            //get next Product ID
+            txtItemId.Text = MenuItem.getNextmenuItemId().ToString("0000");
+
+            cboMenuItemType.Items.Add("F - Food");
+            cboMenuItemType.Items.Add("B - Beverage");
+            cboMenuItemType.Items.Add("D - Dessert");
+
+            
+
+            //Create Data Grid View
+
+            menuItemsDataGridView.ColumnCount = 6;
+
+            menuItemsDataGridView.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
+            menuItemsDataGridView.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            menuItemsDataGridView.ColumnHeadersDefaultCellStyle.Font =
+                new Font(menuItemsDataGridView.Font, FontStyle.Bold);
+
+            menuItemsDataGridView.Name = "menuItemsDataGridView";
+            menuItemsDataGridView.AutoSizeRowsMode =
+                DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            menuItemsDataGridView.ColumnHeadersBorderStyle =
+                DataGridViewHeaderBorderStyle.Single;
+            menuItemsDataGridView.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            menuItemsDataGridView.GridColor = Color.Black;
+            menuItemsDataGridView.RowHeadersVisible = false;
+
+            menuItemsDataGridView.Columns[0].Name = "Availability"; 
+            menuItemsDataGridView.Columns[1].Name = "ID";
+            menuItemsDataGridView.Columns[2].Name = "Name";
+            menuItemsDataGridView.Columns[3].Name = "Type";
+            menuItemsDataGridView.Columns[4].Name = "Description";
+            menuItemsDataGridView.Columns[5].Name = "Price";
+
+            menuItemsDataGridView.Columns[0].DefaultCellStyle.Font =
+                new Font(menuItemsDataGridView.DefaultCellStyle.Font, FontStyle.Italic);
+
+            menuItemsDataGridView.SelectionMode =
+                DataGridViewSelectionMode.FullRowSelect;
+            menuItemsDataGridView.AllowUserToAddRows = false;
+            menuItemsDataGridView.MultiSelect = false;
+
+            //ensure columns span whole DataGrid View Table
+            menuItemsDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            menuItemsDataGridView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            menuItemsDataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            menuItemsDataGridView.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            menuItemsDataGridView.Columns[5].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+
+
+            //Populate Data Grid View with default information
+
+            string[] menuItem1 = { "A - Available", "1", "Spaghetti", "F - Food", "Italian", "15.00" };
+
+            menuItemsDataGridView.Rows.Add(menuItem1);
+        }
+
+        int idCount = 2;
+        private void btnAddMenuItem_Click(object sender, EventArgs e)
+        {
+            //Add to Data Grid View with inputted information ----After validating
+
+            double numberDouble;
+
+            bool priceIsParcable = double.TryParse(txtPrice.Text, out numberDouble);
+
+            bool itemNameContainsDigit = txtItemName.Text.Any(char.IsDigit);
+            bool itemDescriptionContainsDigit = txtItemDescription.Text.Any(char.IsDigit);
+            bool itemNameHasNoSpecial = txtItemName.Text.All(char.IsLetter);
+            bool itemDescriptionHasNoSpecial = txtItemDescription.Text.All(char.IsLetter);
+
+            if (cboMenuItemType.SelectedIndex==-1)
+            {
+                MessageBox.Show("A Menu Item Type must be selected.", "Error!", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                cboMenuItemType.Focus();
+                return;
+            }
+
+            if(txtItemName.Text.Equals(""))
+            {
+                MessageBox.Show("Name must be entered.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtItemName.Focus();
+                return;
+            }
+
+            switch (cboMenuItemType.Text)
+            {
+                case "":
+                    MessageBox.Show("A Menu Item Type must be selected.", "Error!");
+                    break;
+                default:
+
+                    switch (txtItemName.Text)
+                    {
+                        case "":
+                            MessageBox.Show("Item Name cannot be empty.", "Error!");
+                            break;
+                        default:
+
+                            switch (txtItemDescription.Text)
+                            {
+                                case "":
+                                    MessageBox.Show("Item Description cannot be empty.", "Error!");
+                                    break;
+                                default:
+
+                                    switch (itemNameContainsDigit || itemDescriptionContainsDigit)
+                                    {
+                                        case true:
+                                            MessageBox.Show("Item Name and Description cannot include numbers", "Error!");
+                                            break;
+                                        case false:
+                                            switch (itemNameHasNoSpecial || itemDescriptionHasNoSpecial)
+                                            {
+                                                case false:
+                                                    MessageBox.Show("Item Name and Description cannot include special characters", "Error!");
+                                                    break;
+                                                case true:
+                                                    switch (priceIsParcable)
+                                                    {
+                                                        case false:
+                                                            MessageBox.Show("Item Price must be a number, cannot include letters.", "Error!");
+                                                            break;
+                                                        case true:
+                                                            switch (txtPrice.Text)
+                                                            {
+                                                                case "":
+                                                                    MessageBox.Show("Item Price cannot be empty.", "Error!");
+                                                                    break;
+                                                                default:
+                                                                    switch (double.Parse(txtPrice.Text) <= 0)
+                                                                    {
+                                                                        case true:
+                                                                            MessageBox.Show("Item Price must be a positive number.", "Error!");
+                                                                            break;
+                                                                        case false:
+                                                                            string[] newMenuItem = { "A - Available", idCount.ToString(), txtItemName.Text, cboMenuItemType.Text, txtItemDescription.Text, txtPrice.Text };
+                                                                            menuItemsDataGridView.Rows.Add(newMenuItem);
+                                                                            idCount++;
+                                                                            break;
+                                                                    }
+                                                                    break;
+                                                            }
+                                                            break;
+                                                    }
+                                                    break;
+                                            }
+                                            break;
+                                    }
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+            }
+
+            //insert the data into database
+            MenuItem anItem = new MenuItem();
+
+            //Create an instance of Product and instantiate with values from form controls
+            MenuItem aProduct = new MenuItem(Convert.ToInt32(txtItemId.Text), cboMenuItemType.Text,txtItemName.Text, txtItemDescription.Text,
+                Convert.ToDecimal(txtPrice.Text));
+
+            //invoke the method to add the data to the Products table
+            aProduct.addProduct();
+
+            //display confirmation message
+            MessageBox.Show("Product " + txtItemId.Text + " added successfully", "Success",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //reset UI
+            txtItemName.Text = MenuItem.getNextmenuItemId().ToString("0000");
+            txtItemName.Clear();
+            cboMenuItemType.SelectedIndex = -1;
+            txtItemDescription.Clear();
+            txtPrice.Text = "0.00";
+            cboMenuItemType.Focus();
+
+        }
+    }
+}
