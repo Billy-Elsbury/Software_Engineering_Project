ed251cd180d902c3ba932362aaf25608c871a420
Billy <billy.elsbury@students.ittralee.ie>
2023-04-21 15:12:54 +0100

    comm

--- Restaurant System/frmItemAnalysis.Designer.cs
+++ Restaurant System/frmItemAnalysis.Designer.cs
@@ -29,36 +29,75 @@ namespace Restuarant_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBoxMenuItemAnalysis = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMenuItemAnalysis)).BeginInit();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cboSelectedYear = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chtItemAnalysis = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chtItemAnalysis)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBoxMenuItemAnalysis
+            // cboSelectedYear
             // 
-            this.pictureBoxMenuItemAnalysis.Image = global::Restuarant_System.Properties.Resources.ItemSalesGraphRQ;
-            this.pictureBoxMenuItemAnalysis.Location = new System.Drawing.Point(12, 12);
-            this.pictureBoxMenuItemAnalysis.Name = "pictureBoxMenuItemAnalysis";
-            this.pictureBoxMenuItemAnalysis.Size = new System.Drawing.Size(776, 426);
-            this.pictureBoxMenuItemAnalysis.TabIndex = 0;
-            this.pictureBoxMenuItemAnalysis.TabStop = false;
+            this.cboSelectedYear.FormattingEnabled = true;
+            this.cboSelectedYear.Location = new System.Drawing.Point(142, 26);
+            this.cboSelectedYear.Name = "cboSelectedYear";
+            this.cboSelectedYear.Size = new System.Drawing.Size(121, 24);
+            this.cboSelectedYear.TabIndex = 5;
+            this.cboSelectedYear.SelectedIndexChanged += new System.EventHandler(this.cboSelectedYear_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(25, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Select Year:";
+            // 
+            // chtItemAnalysis
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chtItemAnalysis.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chtItemAnalysis.Legends.Add(legend3);
+            this.chtItemAnalysis.Location = new System.Drawing.Point(25, 76);
+            this.chtItemAnalysis.Name = "chtItemAnalysis";
+            this.chtItemAnalysis.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chtItemAnalysis.Series.Add(series3);
+            this.chtItemAnalysis.Size = new System.Drawing.Size(886, 547);
+            this.chtItemAnalysis.TabIndex = 3;
+            this.chtItemAnalysis.Text = "chart1";
             // 
             // frmItemAnalysis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBoxMenuItemAnalysis);
+            this.ClientSize = new System.Drawing.Size(1067, 642);
+            this.Controls.Add(this.cboSelectedYear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chtItemAnalysis);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmItemAnalysis";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu Item Analysis";
             this.Load += new System.EventHandler(this.frmItemAnalysis_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMenuItemAnalysis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtItemAnalysis)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBoxMenuItemAnalysis;
+        private System.Windows.Forms.ComboBox cboSelectedYear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtItemAnalysis;
     }
 }
--- Restaurant System/frmRevenueAnalysis.Designer.cs
+++ Restaurant System/frmRevenueAnalysis.Designer.cs
@@ -29,36 +29,83 @@ namespace Restuarant_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBoxRevenueAnalysis = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRevenueAnalysis)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.chtRevenueAnalysis = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cboSelectedYear = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chtRevenueAnalysis)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBoxRevenueAnalysis
+            // notifyIcon1
             // 
-            this.pictureBoxRevenueAnalysis.Image = global::Restuarant_System.Properties.Resources.RevenueAnalysisGraphRQ;
-            this.pictureBoxRevenueAnalysis.Location = new System.Drawing.Point(12, 12);
-            this.pictureBoxRevenueAnalysis.Name = "pictureBoxRevenueAnalysis";
-            this.pictureBoxRevenueAnalysis.Size = new System.Drawing.Size(776, 426);
-            this.pictureBoxRevenueAnalysis.TabIndex = 0;
-            this.pictureBoxRevenueAnalysis.TabStop = false;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // chtRevenueAnalysis
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chtRevenueAnalysis.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chtRevenueAnalysis.Legends.Add(legend1);
+            this.chtRevenueAnalysis.Location = new System.Drawing.Point(12, 88);
+            this.chtRevenueAnalysis.Name = "chtRevenueAnalysis";
+            this.chtRevenueAnalysis.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chtRevenueAnalysis.Series.Add(series1);
+            this.chtRevenueAnalysis.Size = new System.Drawing.Size(886, 547);
+            this.chtRevenueAnalysis.TabIndex = 0;
+            this.chtRevenueAnalysis.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Select Year:";
+            // 
+            // cboSelectedYear
+            // 
+            this.cboSelectedYear.FormattingEnabled = true;
+            this.cboSelectedYear.Location = new System.Drawing.Point(129, 38);
+            this.cboSelectedYear.Name = "cboSelectedYear";
+            this.cboSelectedYear.Size = new System.Drawing.Size(121, 24);
+            this.cboSelectedYear.TabIndex = 2;
+            this.cboSelectedYear.SelectedIndexChanged += new System.EventHandler(this.cboSelectedYear_SelectedIndexChanged);
             // 
             // frmRevenueAnalysis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBoxRevenueAnalysis);
+            this.ClientSize = new System.Drawing.Size(913, 652);
+            this.Controls.Add(this.cboSelectedYear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chtRevenueAnalysis);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmRevenueAnalysis";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Revenue Analysis";
             this.Load += new System.EventHandler(this.frmRevenueAnalysis_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRevenueAnalysis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtRevenueAnalysis)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBoxRevenueAnalysis;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtRevenueAnalysis;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cboSelectedYear;
     }
 }
